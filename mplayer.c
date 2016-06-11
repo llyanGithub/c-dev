242bbef37ec56906a0db0dc5e8d40be16875a03c
llyan <linglongyan@icloud.com>
2016-06-11 14:35:49 +0000

    add mplayer.c

---
+++ mplayer.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+const char* mplayer_cmd = "mplayer -ao alsa -playlist /home/pi/Music/music.lst";
+
+static pid_t *childpid = NULL;
+
+static int maxfd;
+
+/*
+FILE* me_popen(const char* cmdstring, const char* type)
+{
+	int i;
+	int pfd[2];
+	pid_t pid;
+	FILE* fp;
+
+	if ((type[0] != 'r' && type[0] != 'w') || type[1] != 0) {
+		errno = EINVAL;	
+		return (NULL);
+	}
+
+	if (childpid == NULL) {
+		maxfd = open_max();	
+		if ((childpd = calloc(maxfd, sizeof(pid_t))) == NULL) {
+			return (NULL);		
+		}
+	}
+
+	if (pipe(pfd) < 0)
+		return (NULL);
+
+	if ((pid = fork()) < 0) {
+		return (NULL);
+	} else if (pid == 0) {
+		if (*type == 'r') {
+			close(pfd[0]);	
+			if (pfd[1] != STDOUT_FILENO) {
+				dup2(pfd[1], STDOUT_FILENO);	
+				close(pfd[1]);
+			}
+		} else {
+			close(pfd[1]);	
+			if (pfd[0] != STDIN_FILENO) {
+				dup2(pfd[0], STDIN_FILENO);	
+				close(pfd[0]);
+			}
+		}
+
+		for (i = 0; i < maxfd; i++)
+			if (childpid[i] > 0)
+				close(i);
+
+		execl("/bin/sh", "sh", "-c", cmdstring, (char*)0);
+		exit(127);
+	} 
+
+
+	if (*type == 'r') {
+		close(pfd[1]);	
+		if ((fp =  fdopen(pfd[0], type)) == NULL) {
+			return NULL;
+		} 
+	} else {
+		close(pfd[0]);	
+		if ((fp =  fdopen(pfd[1], type)) == NULL) {
+			return NULL;
+		} 
+	}
+
+	childpid[fileno(fp)] = pid;
+	return(fp);
+}
+*/
+
+void main(void)
+{
+	char cmd;
+	FILE* fp;
+	int ret = -1;
+
+	fp = popen(mplayer_cmd, "w");
+	if (fp == NULL) {
+		printf("poepn error\n");
+		exit(1);
+	}
+
+	while (cmd = getchar() != 'q') {
+		switch (cmd) {
+			case '<':
+			case '>':
+			case '/':
+			case '*':
+				ret = fputc(cmd, fp);
+				if (ret == -1) {
+					printf("fputc error \n");
+					exit(1);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+}
+
